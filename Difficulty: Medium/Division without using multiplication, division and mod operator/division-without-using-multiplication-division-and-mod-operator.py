cb25f91c7af598077e50cd1b1fc2392c7aeb14c6
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-02-10 23:01:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Division without using multiplication, division and mod operator/division-without-using-multiplication-division-and-mod-operator.py
@@ -0,0 +1,52 @@
+# User function Template for python3
+class Solution:
+    def divide(self, dividend, divisor):
+        if divisor == 0:
+            raise ValueError("Cannot divide by zero")
+
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647  
+
+        negative = (dividend < 0) ^ (divisor < 0)  
+        dividend, divisor = abs(dividend), abs(divisor)
+
+        quotient = 0
+        temp_divisor, multiple = divisor, 1
+
+        while (temp_divisor << 1) <= dividend:
+            temp_divisor <<= 1
+            multiple <<= 1
+
+        while dividend >= divisor:
+            if dividend >= temp_divisor:
+                dividend -= temp_divisor
+                quotient += multiple
+            temp_divisor >>= 1  # Reduce divisor
+            multiple >>= 1  # Reduce corresponding quotient value
+
+        return -quotient if negative else quotient
+
+
+    
+        #code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(0, t):
+        inp = list(map(int, input().split()))
+
+        a = inp[0]
+        b = inp[1]
+
+        ob = Solution()
+
+        print(ob.divide(a, b))
+
+        print("~")
+
+# } Driver Code Ends
