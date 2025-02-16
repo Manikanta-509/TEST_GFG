e008a2604359efc77bd7a72f06dfdb66f64fbdce
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-02-16 23:10:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split the binary string into substrings with equal number of 0s and 1s/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.py
@@ -0,0 +1,35 @@
+#Back-end complete function Template for Python 3
+class Solution:
+    def maxSubStr(self, S):
+        count0 = count1 = 0  # Count of 0s and 1s
+        balanced_count = 0  # Number of balanced substrings
+
+        for char in s:
+            if char == "0":
+                count0 += 1
+            else:
+                count1 += 1
+            
+            if count0 == count1:
+                balanced_count += 1  # Found a balanced substring
+        
+        # If total 0s and 1s are not equal, splitting fully is impossible
+        return balanced_count if count0 == count1 else -1
+
+        #Write your code here
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        s=input()
+        obj=Solution()
+        ans=obj.maxSubStr(s)
+        print(ans)
+        print("~")
+# } Driver Code Ends
