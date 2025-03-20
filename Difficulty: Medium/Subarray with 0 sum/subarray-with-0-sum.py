cc369b21202324cdc062a597045cfad2bc0ec7aa
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-03-20 23:34:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    def subArrayExists(self, arr):
+        sum_index_map = {}
+        current_sum = 0
+
+        for i in range(len(arr)):
+            current_sum += arr[i]
+            if current_sum == 0 or current_sum in sum_index_map:
+                return True
+            sum_index_map[current_sum] = i
+        
+        return False
+  #Return true or false
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+        if (Solution().subArrayExists(arr)):
+            print("true")
+        else:
+            print("false")
+
+        T -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
