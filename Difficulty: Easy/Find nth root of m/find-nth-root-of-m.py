9f55169eff5a94213291f2e61e1262665abd7ce3
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-07-12 00:24:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+class Solution:
+	def nthRoot(self, n: int, m: int) -> int:
+        low = 1
+        high = m
+        while low <= high:
+            mid = (low + high) // 2
+            power = mid ** n
+            if power == m:
+                return mid
+            elif power < m:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+
+		# Code here
