b7e2f93e6e4285567bfa8235e393e5b5f554f258
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-07-14 22:51:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.py
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -9,17 +9,4 @@ class Solution:
         
         # Code Here
 
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        ans = ob.getSecondLargest(arr)
-        print(ans)
-        print("~")
-# } Driver Code Ends
+        # Code Here
