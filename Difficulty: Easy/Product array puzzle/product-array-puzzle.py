61884b5b8c0323e0b933546d2bd967e730fac6d6
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-07-18 22:48:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.py
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.py
@@ -4,35 +4,19 @@ class Solution:
     def productExceptSelf(self, arr):
         n = len(arr)
         result = [1] * n
-        prefix = 1
+
+        # Compute left product for each element
+        left = 1
         for i in range(n):
-            result[i] = prefix
-            prefix *= arr[i]
-        
-        suffix = 1
-        for i in range(n-1, -1, -1):
-            result[i] *= suffix
-            suffix *= arr[i]
-        
+            result[i] = left
+            left *= arr[i]
+
+        # Compute right product and multiply with result
+        right = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= right
+            right *= arr[i]
+
         return result
 
-        
         #code here
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-
-    for _ in range(t):
-
-        arr = [int(x) for x in input().split()]
-
-        ans = Solution().productExceptSelf(arr)
-        print(*ans)
-        print("~")
-
-# } Driver Code Ends
