0668a6948a451e9154dc52b8a4b5dfbf4673ed3e
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-07-21 17:13:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.py
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.py
@@ -1,41 +1,14 @@
 #User function Template for python3
 
-#User function Template for python3
-
 class Solution:
     def getMinDiff(self, arr,k):
         arr.sort()
         ans=arr[-1]-arr[0]
         for i in range(len(arr)-1):
-            max_value=max(arr[-1]-k,arr[i]+k)
-            min_value=min(arr[0]+k,arr[i+1]-k)
-            if min_value<0:
+            max_ele=max(arr[-1]-k,arr[i]+k)
+            min_ele=min(arr[0]+k,arr[i+1]-k)
+            if min_ele<0:
                 continue
-            ans=min(ans,max_value-min_value)
+            ans=min(ans,max_ele-min_ele)
         return ans
-        #User function Template for python3
-
-            
         # code here
-
-  
-        # code here
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        k = int(input())
-        # n = int(input())
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        ans = ob.getMinDiff(arr, k)
-        print(ans)
-        print("~")
-        tc -= 1
-
-# } Driver Code Ends
