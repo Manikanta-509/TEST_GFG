a5c7469344d0b640dcbca0b9fcba9feb343dd650
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-07-22 07:05:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.py
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.py
@@ -7,80 +7,15 @@ class node:
         self.next = None
 '''
 class Solution:
-    # Should return data of the middle node. If the list is empty, return -1
+    #  Should return data of middle node. If linked list is empty, then  -1
     def getMiddle(self, head):
         if not head:
-            return -1  # Handle empty list case
-        
-        slow = head
-        fast = head
-        
-        # Move slow and fast pointers
+            return -1
+        slow=head
+        fast=head
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        # When the loop ends, slow points to the middle node
+            slow=slow.next
+            fast=fast.next.next
         return slow.data
-
-            
-            
         # Code here
         # return the value stored in the middle node
-
-
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python3
-
-
-# Node Class
-class node:
-
-    def __init__(self):
-        self.data = None
-        self.next = None
-
-
-# Linked List Class
-class Linked_List:
-
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def insert(self, data):
-        if self.head == None:
-            self.head = node()
-            self.tail = self.head
-            self.head.data = data
-        else:
-            new_node = node()
-            new_node.data = data
-            new_node.next = None
-            self.tail.next = new_node
-            self.tail = self.tail.next
-
-
-def printlist(head):
-    temp = head
-    while temp is not None:
-        print(temp.data, end=" ")
-        temp = temp.next
-    print('')
-
-
-# Driver Program
-if __name__ == '__main__':
-    t = int(input())
-    for i in range(t):
-        list1 = Linked_List()
-        arr = list(map(int, input().strip().split()))
-        for i in arr:
-            list1.insert(i)
-        ob = Solution()
-        print(ob.getMiddle(list1.head))
-        print("~")
-
-# } Driver Code Ends
