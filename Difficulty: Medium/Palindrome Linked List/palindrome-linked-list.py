d1454f263ee9c8e187e773b5d25de38d4f6a05b2
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-07-22 07:18:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.py
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.py
@@ -1,13 +1,13 @@
 #User function Template for python3
 '''
 
-	{
-		# Node Class
-		class Node:
-		    def __init__(self, data):   # data -> value stored in node
-		        self.data = data
-		        self.next = None
-	}
+    {
+        # Node Class
+        class Node:
+            def __init__(self, data):   # data -> value stored in node
+                self.data = data
+                self.next = None
+    }
 
 '''
 #Function to check whether the list is palindrome.
@@ -32,53 +32,3 @@ class Solution:
             right = right.next
 
         return True
-
-
-#{ 
- # Driver Code Starts
-#main
-
-
-class Node:
-    # Constructor to initialize the node object
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class LinkedList:
-    # Function to initialize head
-    def __init__(self):
-        self.head = None
-
-    # Function to insert a new node at the beginning
-    def push(self, new_data):
-        new_node = Node(new_data)
-        new_node.next = self.head
-        self.head = new_node
-
-    # Utility function to prit the linked LinkedList
-    def printList(self):
-        temp = self.head
-        while (temp):
-            print(temp.data, end=" ")
-            temp = temp.next
-        print("")
-
-
-if __name__ == '__main__':
-    t = int(input())
-    while (t > 0):
-        llist = LinkedList()
-        values = input().strip().split()
-        for i in reversed(values):
-            llist.push(i)
-        flag = Solution().isPalindrome(llist.head)
-        if flag:
-            print("true")
-        else:
-            print("false")
-        t -= 1
-        print("~")
-
-# } Driver Code Ends
