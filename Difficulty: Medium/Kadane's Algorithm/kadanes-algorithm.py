d16a39abb0408f877dcf12344ff15268aff2d67d
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-08-23 23:14:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxSubarraySum(self, arr):
-        max_le=min_le=mid=arr[0]
+        left=right=arr[0]
         for i in range(1,len(arr)):
-            max_le=max(arr[i],max_le+arr[i])
-            min_le=max(max_le,min_le)
-        return min_le
+            left=max(arr[i],arr[i]+left)
+            right=max(right,left)
+        return right
+        
         # Code here
