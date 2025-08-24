e65744db917fb6528b693056061db3aeb334bf35
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-08-24 21:57:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.py
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.py
@@ -1,24 +1,21 @@
-#User function Template for python3
 '''
-        # Node Class
-        class Node:
-            def __init__(self, data):   # data -> value stored in node
-                self.data = data
-                self.next = None
-    
+    # Node Class
+    class Node:
+		 def __init__(self, data):
+    		 self.data = data
+    		 self.next = None
+	
 '''
-# Return boolean value True or False
 
 class Solution:
     #Function to check if the linked list has a loop.
     def detectLoop(self, head):
         slow=head
-        fast=head
+        fast=head.next
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
             if slow==fast:
                 return True
         return False
-    
         #code here
