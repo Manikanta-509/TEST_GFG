51793037fae094394351dee743e490030a762a67
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-09-15 21:09:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count BST nodes that lie in a given range/count-bst-nodes-that-lie-in-a-given-range.py
@@ -0,0 +1,28 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+class Solution:
+    def getCount(self, root, l, h):
+        if not root:
+            return 0
+        
+        # If root is in range
+        if l <= root.data <= h:
+            return (1 
+                    + self.getCount(root.left, l, h) 
+                    + self.getCount(root.right, l, h))
+        
+        # If root's value is smaller than l, only right subtree may have nodes
+        elif root.data < l:
+            return self.getCount(root.right, l, h)
+        
+        # If root's value is greater than h, only left subtree may have nodes
+        else:
+            return self.getCount(root.left, l, h)
+
+
+        # Your code here
