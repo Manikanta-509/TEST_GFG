a14a404e6e9a8fa09090007dfc12c33a45699a1b
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-10-21 14:05:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotate Array by One/rotate-array-by-one.py
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.py
@@ -2,8 +2,13 @@
 
 class Solution:
     def rotate(self, arr):
-        a=arr.pop()
-        arr.insert(0,a)
+        n=len(arr)
+        if n==0:
+            return arr
+        last=arr[-1]
+        for i in range(n-1,0,-1):
+            arr[i]=arr[i-1]
+        arr[0]=last
         return arr
     
 
