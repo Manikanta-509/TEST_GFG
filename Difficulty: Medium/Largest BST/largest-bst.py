b96bfb1e4330505c9f8bf6c4ed81faab3c9c1bcb
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-10-22 21:46:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.py
+++ Difficulty: Medium/Largest BST/largest-bst.py
@@ -1,24 +1,15 @@
 class Solution:
     def largestBst(self, root):
         self.max_size = 0
-
         def dfs(node):
-            if node is None:
-                # For null nodes: it's a valid BST with size 0
-                return True, 0, float('inf'), float('-inf')
-            
-            # Recursively check left and right subtrees
-            l_bst, l_size, l_min, l_max = dfs(node.left)
-            r_bst, r_size, r_min, r_max = dfs(node.right)
-
-            # Check if current subtree is BST
-            if l_bst and r_bst and l_max < node.data < r_min:
-                size = l_size + r_size + 1
-                self.max_size = max(self.max_size, size)
-                return True, size, min(l_min, node.data), max(r_max, node.data)
-            
-            # If not BST, return False
-            return False, 0, 0, 0
-
+            if not node:
+                return True,float('inf'),float('-inf'),0
+            l_dfs,l_min,l_max,l_size=dfs(node.left)
+            r_dfs,r_min,r_max,r_size=dfs(node.right)
+            if l_dfs and r_dfs and l_max<node.data<r_min:
+                size=l_size+r_size+1
+                self.max_size=max(self.max_size,size)
+                return True,min(l_min,node.data),max(r_max,node.data),size
+            return False,0,0,0
         dfs(root)
         return self.max_size
