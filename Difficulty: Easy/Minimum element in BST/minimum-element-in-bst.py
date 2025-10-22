d289a2f0b3f5608a1e5dd99b101448b90eac0ba1
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2025-10-22 21:59:56 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum element in BST/minimum-element-in-bst.py
@@ -0,0 +1,29 @@
+"""
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+"""
+"""
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+"""
+class Solution:
+    # Function to find the minimum element in the given BST.
+    def minValue(self, root):
+        if root is None:
+            return -1   # If the tree is empty
+        
+        curr = root    # Start at root
+        while curr.left is not None:  # Keep moving left
+            curr = curr.left
+        
+        return curr.data   # Return the leftmost node’s value
+
+            
+        ##Your code here
+        # code here
