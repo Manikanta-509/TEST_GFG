825964705ce5578233597d0d32fa3867b874e09e
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-02-09 17:03:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Int Str/int-str.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+a = input()
+b = input()
+c = input()
+d=int(a)
+e=int(b)
+print(d*a+c+e*b)
+# Write your code below that prints a a times and b b times, seperated by c
