c4ad9ac9817e5c46593fd899b65196f20b801b96
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-02-28 13:58:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of Array/gcd-of-array.py
+++ Difficulty: Basic/GCD of Array/gcd-of-array.py
@@ -1,16 +1,13 @@
 #User function Template for python3
 
 class Solution:
-    def find(self, a, b):
-        while b != 0:
-            a, b = b, a % b
+    def find(self,a,b):
+        while b!=0:
+            a,b=b,a%b
         return a
-
     def gcd(self, n, arr):
-        result = arr[0]
-        for i in range(1, n):
-            result = self.find(result, arr[i])
+        result=arr[0]
+        for i in range(1,n):
+            result=self.find(result,arr[i])
         return result
-
-                
         # code here 
