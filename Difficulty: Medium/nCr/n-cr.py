decd0af33aa21488becd1a2d733786868881bf45
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-02-28 14:14:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/nCr/n-cr.py
+++ Difficulty: Medium/nCr/n-cr.py
@@ -1,11 +1,11 @@
 class Solution:
-    def factorial(self,n):
-        result=1
+    def fact(self,n):
+        fact=1
         for i in range(2,n+1):
-            result*=i
-        return result
+            fact*=i
+        return fact
     def nCr(self, n, r):
         if r>n:
             return 0
-        return self.factorial(n)//(self.factorial(n-r)*self.factorial(r))
-        # code herehttps://media.geeksforgeeks.org/img-practice/chatIcon-1653561581.svg
+        return self.fact(n)//(self.fact(n-r)*self.fact(r))
+        # code here
