f2a00cd7f7a169a43c12ce105c403fd544945f03
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-02-28 13:55:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
@@ -1,8 +1,9 @@
 class Solution:
     def lcmAndGcd(self, a : int, b : int) -> List[int]:
-        org1,org2=a,b
+        org1=a
+        org2=b
         while b!=0:
-            a,b=b, a%b
+            a,b=b,a%b
         gcd=a
         lcm=(org1*org2)//gcd
         return [lcm,gcd]
