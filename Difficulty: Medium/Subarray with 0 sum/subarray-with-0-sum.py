9f7570bf05a23d80f6f06b0283de54fce22559b6
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-03-02 17:24:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.py
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.py
@@ -1,38 +1,16 @@
 #User function Template for python3
 
 class Solution:
-    def subArrayExists(self, arr):
-        sum_index_map = {}
-        current_sum = 0
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr):
+        sub_array={}
+        current=0
         for i in range(len(arr)):
-            current_sum += arr[i]
-            if current_sum == 0 or current_sum in sum_index_map:
+            current+=arr[i]
+            if current==0 or current in sub_array:
                 return True
-            sum_index_map[current_sum] = i
+            sub_array[current]=i
         return False
-  #Return true or false
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-    T = int(input())
-    while (T > 0):
-
-        arr = [int(x) for x in input().strip().split()]
-        if (Solution().subArrayExists(arr)):
-            print("true")
-        else:
-            print("false")
-
-        T -= 1
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+        ##Your code here
+        #Return true or false
