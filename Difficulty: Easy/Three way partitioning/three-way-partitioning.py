485662b988cbfa855058421d5927315b5ec25bb6
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-03-03 16:47:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Three way partitioning/three-way-partitioning.py
+++ Difficulty: Easy/Three way partitioning/three-way-partitioning.py
@@ -1,17 +1,21 @@
+#User function template for Python
+
 class Solution:
-    def threeWayPartition(self, arr, a, b):
-        n = len(arr)
-        low, mid, high = 0, 0, n - 1
-        
-        while mid <= high:
-            if arr[mid] < a:
-                arr[low], arr[mid] = arr[mid], arr[low]
-                low += 1
-                mid += 1
-            elif arr[mid] > b:
-                arr[mid], arr[high] = arr[high], arr[mid]
-                high -= 1
-            else:
-                mid += 1
-        
-        return arr
+    #Function to partition the array around the range such 
+    #that array is divided into three parts.
+	def threeWayPartition(self, arr, a, b):
+	    low=0
+	    mid=0
+	    high=len(arr)-1
+	    while mid<=high:
+	        if arr[mid]<a:
+	            arr[low],arr[mid]=arr[mid],arr[low]
+	            mid+=1
+	            low+=1
+	        elif arr[mid]>b:
+	            arr[mid],arr[high]=arr[high],arr[mid]
+	            high-=1
+	        else:
+	            mid+=1
+	        
+	    # code here 
