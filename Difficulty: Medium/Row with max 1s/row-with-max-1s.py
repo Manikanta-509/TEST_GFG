4654612526c48ecf5ae5061e147a4411ef971555
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-03-04 11:00:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.py
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.py
@@ -1,10 +1,9 @@
 class Solution:
     def rowWithMax1s(self, arr):
-        row_index=-1
+        roe_index=-1
         j=len(arr[0])-1
         for i in range(len(arr)):
             while j>=0 and arr[i][j]==1:
                 j-=1
-                row_index=i
-        return row_index
-
+                roe_index=i
+        return roe_index
