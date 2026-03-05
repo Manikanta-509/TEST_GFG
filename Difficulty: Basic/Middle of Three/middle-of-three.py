7f01b1589203c33ce2947f7cbef4796fb3662f9b
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-03-05 16:21:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Middle of Three/middle-of-three.py
+++ Difficulty: Basic/Middle of Three/middle-of-three.py
@@ -2,25 +2,7 @@
 
 class Solution:
     def middle(self, a, b, c):
-        nums=[a,b,c]
-        nums.sort()
-        return nums[1]
+        a=[a,b,c]
+        a.sort()
+        return a[1]
         #code here
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        A = int(input())
-        B = int(input())
-        C = int(input())
-        ob = Solution()
-        print(ob.middle(A, B, C))
-        print("~")
-
-# } Driver Code Ends
