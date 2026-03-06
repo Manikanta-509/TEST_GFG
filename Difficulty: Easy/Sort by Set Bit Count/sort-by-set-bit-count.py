f1d873328c5d384cab71482b2599ce7192fd0ed2
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-03-06 18:30:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort by Set Bit Count/sort-by-set-bit-count.py
+++ Difficulty: Easy/Sort by Set Bit Count/sort-by-set-bit-count.py
@@ -2,9 +2,6 @@
 
 class Solution:
     def sortBySetBitCount(self, arr, n):
-        # Sort by number of set bits in descending order
         arr.sort(key=lambda x: bin(x).count('1'), reverse=True)
         return arr
-
-        
         # Your code goes here
