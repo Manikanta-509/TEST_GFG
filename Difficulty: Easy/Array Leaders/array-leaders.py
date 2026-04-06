0a1086d986b725460d777f52712bf1519d0ff766
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-04-06 21:44:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.py
+++ Difficulty: Easy/Array Leaders/array-leaders.py
@@ -1,18 +1,11 @@
 class Solution:
     def leaders(self, arr):
-        n = len(arr)
-        leaders = []
-        max_from_right = arr[-1]
-        leaders.append(max_from_right)
-
-        # Traverse from second last to the start
-        for i in range(n - 2, -1, -1):
-            if arr[i] >= max_from_right:
-                max_from_right = arr[i]
-                leaders.append(arr[i])
-
-        # Reverse the list to maintain original order of leaders
-        leaders.reverse()
-        return leaders
-
-        # code here
+        n=len(arr)
+        max_leader=arr[-1]
+        leader=[max_leader]
+        for i in range(n-2,-1,-1):
+            if arr[i]>=max_leader:
+                max_leader=arr[i]
+                leader.append(arr[i])
+        leader.reverse()
+        return leader
