770b5db032d7a69df2101b1813d336d2aa9b4426
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-05-21 20:56:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.py
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.py
@@ -1,12 +1,10 @@
 class Solution:
     def count(self, coins, sum):
-        # code here 
-        dp = [0] * (sum + 1)
-        dp[0] = 1   # base case: 1 way to make sum 0
-
-        # For each coin, update ways to form sums
+        # code here  
+        dp=[0]*(sum+1)
+        dp[0]=1
         for coin in coins:
-            for s in range(coin,sum + 1):
-                dp[s] += dp[s - coin]
-
+            for i in range(coin,sum+1):
+                dp[i]+=dp[i-coin]
         return dp[sum]
+            
