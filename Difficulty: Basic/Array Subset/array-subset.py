90e480a10b4035e729f9bf805e12414784f347b6
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-06-29 14:29:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Subset/array-subset.py
+++ Difficulty: Basic/Array Subset/array-subset.py
@@ -1,50 +1,23 @@
-#User function Template for python3
 class Solution:
     def isSubset(self, a, b):
-        # Dictionary to store the frequency of elements in 'a'
-        freq_a = {}
-        for num in a:
-            freq_a[num] = freq_a.get(num, 0) + 1
+        freq = {}
 
-        # Check if each element in 'b' appears in 'a' with sufficient count
-        for num in b:
-            if freq_a.get(num, 0) == 0:
+        # Count frequency of elements in a
+        for i in a:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+
+        # Check elements of b
+        for i in b:
+            if i not in freq or freq[i] == 0:
                 return False
-            freq_a[num] -= 1
-        
+            freq[i] -= 1
+
         return True
-
-
+        # Your code here
     
     
     
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-
-def main():
-
-    T = int(input())
-
-    while (T > 0):
-        a1 = [int(x) for x in input().strip().split()]
-        a2 = [int(x) for x in input().strip().split()]
-        ob = Solution()
-        if ob.isSubset(a1, a2):
-            print("true")
-        else:
-            print("false")
-
-        T -= 1
-
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+    
