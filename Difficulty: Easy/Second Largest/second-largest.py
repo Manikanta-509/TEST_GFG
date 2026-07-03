d2f6c0da54261ccb4ca314250b6fd1f10d105411
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-03 16:22:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.py
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -2,10 +2,12 @@ class Solution:
     def getSecondLargest(self, arr):
         max1=float('-inf')
         max2=float('-inf')
-        for i in arr:
-            if i>max1:
+        for i in range(len(arr)):
+            if arr[i]>max1:
                 max2=max1
-                max1=i
-            elif i>max2 and i!=max1:
-                max2=i
-        return max2 if max2!=float('-inf') else -1
+                max1=arr[i]
+            elif arr[i]>max2 and arr[i]!=max1:
+                max2=arr[i]
+        if max2==float('-inf'):
+                return -1
+        return max2
