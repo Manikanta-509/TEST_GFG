cd72ac076c08dc7011e5a74efebecf4d4f0719d3
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-03 15:31:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.py
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.py
@@ -1,11 +1,13 @@
 class Solution:
 	def maxProduct(self,arr):
-	    min_ele=max_ele=product=arr[0]
+	    min_pr=max_pr=pr=arr[0]
 	    for i in range(1,len(arr)):
 	        if arr[i]<0:
-	            max_ele,min_ele=min_ele,max_ele
-	        max_ele=max(arr[i],arr[i]*max_ele)
-	        min_ele=min(arr[i],arr[i]*min_ele)
-	        product=max(max_ele,min_ele,product)
-	    return product
+	            max_pr,min_pr=min_pr,max_pr
+	        max_pr=max(arr[i],arr[i]*max_pr)
+	        min_pr=min(arr[i],arr[i]*min_pr)
+	        pr=max(pr,max_pr,min_pr)
+	    return pr
+	            # code here
 		# code here
+		
