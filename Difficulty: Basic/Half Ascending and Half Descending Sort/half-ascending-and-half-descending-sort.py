9ec7ce5381c64677565e2ee02caee668d26470fd
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-07 13:50:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Half Ascending and Half Descending Sort/half-ascending-and-half-descending-sort.py
@@ -0,0 +1,7 @@
+class Solution:
+    def customSort(self, arr):
+        n = len(arr)
+        arr[:n//2] = sorted(arr[:n//2])
+        arr[n//2:] = sorted(arr[n//2:], reverse=True)
+        return arr
+        
