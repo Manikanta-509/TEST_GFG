0c9c5ae0c8571946759461edaa18d810f8b01269
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-08 14:36:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Perfect Numbers/perfect-numbers.py
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.py
@@ -1,14 +1,14 @@
 class Solution:
     def isPerfect(self, n):
-        if n <= 1:
-            return False
+        total=0
+        for i in range(1,int(n**0.5)+1):
+            if n%i==0:
+                if i*i==n or i==1:
+                    total+=i
+                else:
+                    total+=i+n//i
+        return total==n
+            
+            
+        # code here 
         
-        total = 1  # 1 is always a proper divisor (exclude n itself)
-        i = 2
-        while i * i <= n:
-            if n % i == 0:
-                total += i
-                if i != n // i:
-                    total += n // i
-            i += 1
-        return total == n
