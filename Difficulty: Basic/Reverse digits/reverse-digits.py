daaa9c00ff7bb961ef1350c7a3f3c36b5bcfa875
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-08 15:39:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse digits/reverse-digits.py
+++ Difficulty: Basic/Reverse digits/reverse-digits.py
@@ -1,11 +1,11 @@
-#User function Template for python3
-
 class Solution:
 	def reverseDigits(self, n):
 	    rev=0
 	    while n>0:
-	        total=n%10
-	        rev=rev*10+total
-	        n=n//10
+	        digit=n%10
+	        rev=rev*10+digit
+	        n//=10
 	    return rev
+	    
 		# Code here
+		
