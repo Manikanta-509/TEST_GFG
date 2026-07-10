56ce8044148ea5f380e67bc9bcaef56633acf9f5
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-10 20:41:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search array with adjacent diff at most k/search-array-with-adjacent-diff-at-most-k.py
+++ Difficulty: Easy/Search array with adjacent diff at most k/search-array-with-adjacent-diff-at-most-k.py
@@ -1,9 +1,10 @@
-#User function Template for python3
-
 class Solution:
     def findStepKeyIndex(self, arr, k, x):
         for i in range(len(arr)):
             if arr[i]==x:
                 return i
+                break
         return -1
+
+        
         # code here
