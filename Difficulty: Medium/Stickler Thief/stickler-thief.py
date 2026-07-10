bbdfccf568642695d66df49ef095eecb698272f2
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-10 20:57:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.py
+++ Difficulty: Medium/Stickler Thief/stickler-thief.py
@@ -1,11 +1,10 @@
-class Solution:
+class Solution:  
     def findMaxSum(self, arr):
-        incl = 0  # loot including previous house
-        excl = 0  # loot excluding previous house
-
-        for money in arr:
-            new_excl = max(incl, excl)
-            incl = excl + money
-            excl = new_excl
-
-        return max(incl, excl)
+        incl=0
+        excul=0
+        for i in arr:
+            new_incl=max(incl,excul)
+            incl=i+excul
+            excul=new_incl
+        return max(incl,excul)
+        
