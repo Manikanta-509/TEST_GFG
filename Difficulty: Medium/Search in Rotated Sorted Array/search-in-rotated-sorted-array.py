dbe5b0e01de555e5259ee8b4b403287f57dfab69
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-10 14:30:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def search(self, arr, key):
+        for i in range(len(arr)):
+            if arr[i]==key:
+                return i
+        return -1
+        # code here
+        
