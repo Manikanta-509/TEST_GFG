828f0c566d127b2fa9b60d4b455f721c974f8e02
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-18 15:05:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Perfect Numbers/perfect-numbers.py
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.py
@@ -3,7 +3,7 @@ class Solution:
         total=0
         for i in range(1,int(n**0.5)+1):
             if n%i==0:
-                if i*i==n or i==1:
+                if i**i==n or i==1:
                     total+=i
                 else:
                     total+=i+n//i
