b53d59aba379e2afcb2f960e9b2722997c58f534
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-20 16:18:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted Insert Position/sorted-insert-position.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsertK(self, arr, k):
+        low=0
+        high=len(arr)-1
+        ans=len(arr)
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid]>=k:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
+        
