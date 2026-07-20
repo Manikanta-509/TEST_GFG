9788a1dd8a819a0acb258c262376640c9913fac1
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-20 13:52:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Middle of Three/middle-of-three.py
+++ Difficulty: Basic/Middle of Three/middle-of-three.py
@@ -2,8 +2,7 @@ class Solution:
     def middle(self, a, b, c):
         if (a>b and a<c) or (a>c and a<b):
             return a
-        elif (b>a and b<c) or (b>c and b<a):
+        elif (b>c and b<a) or (b>a and b<c):
             return b
         else:
             return c
-        #code here
