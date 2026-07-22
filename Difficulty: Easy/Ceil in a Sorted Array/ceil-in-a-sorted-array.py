f4e4a4929f46a206302f350971f0080d9d04d08b
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-22 15:46:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findCeil(self, arr, x):
+        low=0
+        high=len(arr)-1
+        ans=-1
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid]>=x:
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
+        # code here
