3cedd71be59a7f1f0655ce19133c5d1793a5d721
PASUNURI MANIKANTA <pasunurimanikanta@gmail.com>
2026-07-24 15:45:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.py
+++ Difficulty: Medium/Next Permutation/next-permutation.py
@@ -1,35 +1,20 @@
-
 class Solution:
     def nextPermutation(self, arr):
-        n=len(arr)
         pivot=-1
+        n=len(arr)
         for i in range(n-2,-1,-1):
             if arr[i]<arr[i+1]:
                 pivot=i
                 break
         if pivot==-1:
             arr.reverse()
-            return arr
+            return
         for i in range(n-1,pivot,-1):
             if arr[i]>arr[pivot]:
                 arr[i],arr[pivot]=arr[pivot],arr[i]
                 break
         arr[pivot+1:]=reversed(arr[pivot+1:])
         return arr
-
-
-
-
-
                 
-
-                
-            
         # code here
-
-
-                
-
-                
-            
-        # code here
+        
